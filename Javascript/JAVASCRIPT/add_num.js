95ae31bdcb1b75954a3d9e0b183f0ac56a14b78c
kajal singh <141702878+singhkajal9372@users.noreply.github.com>
2023-12-25 12:04:08 +0530

    Delete Javascript directory

--- Javascript/JAVASCRIPT/add_num.js
+++
@@ -1,7 +0,0 @@
-function add() {
-    var f = parseInt(document.getElementById("first").value);
-    var s = parseInt(document.getElementById("second").value);
-
-    var add = f+s;
-    document.getElementById("ans").value = add;
-}
--- Javascript/slice.js
+++
@@ -1,7 +0,0 @@
-let arr = [1,2,3,4,5];
-
-let slice1 = arr.slice(0,2)
-console.log(slice1);
-
-let slice2 = arr.slice(1,5)
-console.log(slice2);
